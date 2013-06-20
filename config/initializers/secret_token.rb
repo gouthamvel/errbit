99e9038423879afba52f419d53bdc3fbf7695e6c
goutham vel <gouthamvel@gmail.com>
2013-06-20 16:52:03 +0530

    correct secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
 
-Errbit::Application.config.secret_token = YAML.load_file(Rails.root.join("config","secret_token.yml"))[:key]
+Errbit::Application.config.secret_token = YAML.load_file(Rails.root.join("config","secret_token.yml"))['key']
